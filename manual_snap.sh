8af4e8d431b2fa859246ddda9f1727d72c605bb5
srilakshmi-cavnue <141760901+srilakshmi-cavnue@users.noreply.github.com>
2024-10-14 17:56:29 -0700

    Update manual_snap.sh

--- manual_snap.sh
+++ manual_snap.sh
@@ -3,5 +3,5 @@
 # for reference - 16,700,000 ns = 16.7ms
 # To call this script -> ./manual_snap.sh 1 167 img1 , 80, 330
 
-gst-launch-1.0 nvarguscamerasrc silent=false  sensor-id=0 ee-mode=0 tnr-mode=0 aeantibanding=0 wbmode=0 gainrange="$n $n" exposurecompensation=0 exposuretimerange="$2000000 $200000" ispdigitalgainrange="1 1" ! "video/x-raw(memory:NVMM),width=3840,height=2160,framerate=(fraction)10/1" ! nvvidconv flip-method=0 ! pngenc ! filesink location="Snap_$3_${n}x_$200us.png"
+gst-launch-1.0 nvarguscamerasrc silent=false sensor-id=0 num-buffers=1 ee-mode=0 tnr-mode=0 tnr-strength=0 aeantibanding=0 wbmode=0 gainrange="$n $n" exposurecompensation=0 exposuretimerange="$2000000 $200000" ispdigitalgainrange="1 1" ! "video/x-raw(memory:NVMM),width=3840,height=2160,framerate=(fraction)10/1" ! nvvidconv flip-method=0 ! pngenc ! filesink location="Snap_$3_${n}x_$200us.png"
 
