a80df2b28fd89d27c21a4527aaa42b9fc45d051d
srilakshmi-cavnue <141760901+srilakshmi-cavnue@users.noreply.github.com>
2024-10-16 13:54:10 -0700

    Update v4l2_raw_sweep.sh 2

--- v4l2_raw_sweep.sh
+++ v4l2_raw_sweep.sh
@@ -1,20 +1,20 @@
 #$1 - filename
 
-./v4l2_raw_snap.sh 8 80 $1
+./v4l2_raw_snap.sh 8 8 $1
 sleep 10
-./v4l2_raw_snap.sh 8 167 $1
+./v4l2_raw_snap.sh 8 16 $1
 sleep 10
-./v4l2_raw_snap.sh 8 330 $1
+./v4l2_raw_snap.sh 8 33 $1
 sleep 10
-./v4l2_raw_snap.sh 16 80 $1
+./v4l2_raw_snap.sh 16 8 $1
 sleep 10
-./v4l2_raw_snap.sh 16 167 $1
+./v4l2_raw_snap.sh 16 16 $1
 sleep 10
-./v4l2_raw_snap.sh 16 330 $1
+./v4l2_raw_snap.sh 16 33 $1
 sleep 10
-./v4l2_raw_snap.sh 23 80 $1
+./v4l2_raw_snap.sh 23 8 $1
 sleep 10
-./v4l2_raw_snap.sh 23 167 $1
+./v4l2_raw_snap.sh 23 16 $1
 sleep 10
-./v4l2_raw_snap.sh 23 330 $1
+./v4l2_raw_snap.sh 23 33 $1
 
