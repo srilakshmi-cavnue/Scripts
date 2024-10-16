bb2f669e282f15d160b521faf7a5f3765c466e3a
srilakshmi-cavnue <141760901+srilakshmi-cavnue@users.noreply.github.com>
2024-10-16 13:54:45 -0700

    Update manual_snap.sh 3

--- manual_snap.sh
+++ manual_snap.sh
@@ -1,7 +1,7 @@
 # $1 = analog gain, $2 = exp in ms, $3 = filename
 # num-buffers=1 default [can be increased for AEC to converge]
 # for reference - 16,700,000 ns = 16.7ms
-# To call this script -> ./manual_snap.sh 1 167 img1 , 80, 330
+# To call this script -> ./manual_snap.sh 1 167 img1 , 8, 33
 
-gst-launch-1.0 nvarguscamerasrc silent=false sensor-id=0 num-buffers=1 ee-mode=0 tnr-mode=0 tnr-strength=0 aeantibanding=0 wbmode=0 gainrange="$1 $1" saturation=1 exposurecompensation=0 exposuretimerange="$200000 $200000" ispdigitalgainrange="1 1" ! "video/x-raw(memory:NVMM),width=3840,height=2160,framerate=(fraction)10/1" ! nvvidconv flip-method=0 ! pngenc ! filesink location="Snap_$3_$1x_$200us.png"
+gst-launch-1.0 nvarguscamerasrc silent=false sensor-id=0 num-buffers=1 ee-mode=0 tnr-mode=0 tnr-strength=0 aeantibanding=0 wbmode=0 gainrange="$1 $1" saturation=1 exposurecompensation=0 exposuretimerange="$2000000 $2000000" ispdigitalgainrange="1 1" ! "video/x-raw(memory:NVMM),width=3840,height=2160,framerate=(fraction)10/1" ! nvvidconv flip-method=0 ! pngenc ! filesink location="Snap_$3_$1x_$2ms.png"
 
